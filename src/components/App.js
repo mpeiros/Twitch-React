71bad1f29de8689fc4d884cd92fdca6d95457d96
Max Peiros <mpeiros@gmail.com>
2017-09-06 18:57:23 -0700

    Add request to fetch and then render top games

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class App extends Component {
+  state = {
+    topGames: []
+  };
+
+  componentDidMount() {
+    axios.get('http://localhost:5000/api/games/top')
+      .then(response => this.setState({ topGames: response.data.top }));
+  }
+
+  renderGames() {
+    return this.state.topGames.map(gameData => {
+      return (
+        <div key={gameData.game._id}>
+          <img src={gameData.game.box.medium} alt="Game Box Art"/>
+          <p>{gameData.game.name}</p>
+          <p>{gameData.viewers}</p>
+        </div>
+      );
+    });
+  }
+
   render() {
     return (
       <div>
-        <h1>Hello World</h1>
+        {this.renderGames()}
       </div>
     );
   }
