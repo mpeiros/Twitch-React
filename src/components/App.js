100f6a8a1885c7bd41d2c78beec3290e78326996
Max Peiros <mpeiros@gmail.com>
2017-09-25 17:50:43 -0700

    Add react-router-dom and set up root route

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 import GameList from './GameList';
 
 class App extends Component {
   render() {
     return (
       <div className="container">
-        <GameList />
+        <BrowserRouter>
+          <Route exact path="/" component={GameList} />          
+        </BrowserRouter>
       </div>
     );
   }
